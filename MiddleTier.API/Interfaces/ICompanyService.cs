0d3ab7747153e85dee4611aef5f72b8e373f091d
kennergf <kenner.g.figueiredo@gmail.com>
2021-10-20 00:04:56 +0100

    Added decoration to the Interfaces

--- MiddleTier.API/Interfaces/ICompanyService.cs
+++ MiddleTier.API/Interfaces/ICompanyService.cs
@@ -8,14 +8,39 @@ namespace MiddleTier.API.Interfaces
 {
     public interface ICompanyService : IDisposable
     {
+        /// <summary>
+        /// Retrieve Company based on ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         Task<CustomResponse<CompanyViewModel>> GetById(Guid id);
         
+        /// <summary>
+        /// Retrieve Company based on ISIN
+        /// </summary>
+        /// <param name="isin"></param>
+        /// <returns></returns>
         Task<CustomResponse<CompanyViewModel>> GetByISIN(string isin);
 
+        /// <summary>
+        /// Add new Company to the system
+        /// </summary>
+        /// <param name="company"></param>
+        /// <returns></returns>
         Task<CustomResponse<CompanyViewModel>> Add(CompanyViewModel company);
 
+        /// <summary>
+        /// Update existing Company on the system
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="company"></param>
+        /// <returns></returns>
         Task<CustomResponse<CompanyViewModel>> Update(Guid id, CompanyViewModel company);
 
+        /// <summary>
+        /// Return a list with all the companies registered on the system
+        /// </summary>
+        /// <returns></returns>
         Task<CustomResponse<IEnumerable<CompanyViewModel>>> GetAll();
     }
 }
--- MiddleTier.API/Interfaces/INotifier.cs
+++ MiddleTier.API/Interfaces/INotifier.cs
@@ -5,10 +5,22 @@ namespace MiddleTier.API.Interfaces
 {
     public interface INotifier
     {
+        /// <summary>
+        /// Check if there is any notification
+        /// </summary>
+        /// <returns></returns>
         bool HasNotification();
 
+        /// <summary>
+        /// Get all notifications available
+        /// </summary>
+        /// <returns></returns>
         List<Notification> GetNotifications();
 
+        /// <summary>
+        /// Add new Notification
+        /// </summary>
+        /// <param name="notification"></param>
         void Add(Notification notification);
     }
 }
